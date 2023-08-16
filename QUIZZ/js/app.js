27d8057d09c0e4d028da42ba291a0b9783caafad
claramcms <127965051+claramcms@users.noreply.github.com>
2023-08-16 16:38:35 -0300

    Add files via upload

---
+++ QUIZZ/js/app.js
@@ -0,0 +1,53 @@
+//Função pergunta 1
+const resultado = document.getElementById("resultado")
+function verificarResposta__1(resposta) {
+    if (resposta === "b"){
+        resultado.innerHTML = "Resposta correta";
+        resultado.style.color = "green"
+    } 
+    else{
+        resultado.innerHTML = "Resposta correta";
+        resultado.style.color = "red"
+    }
+}
+
+
+//Função pergunta 2
+function verificarResposta__2(resposta) {
+    if (resposta === "c"){
+        resultado.innerHTML = "Resposta correta";
+        resultado.style.color = "green"
+    } 
+    else{
+        resultado.innerHTML = "Resposta correta";
+        resultado.style.color = "red"
+    }
+}
+
+
+//Função pergunta 3
+function verificarResposta__3(resposta) {
+    if (resposta === "d"){
+        resultado.innerHTML = "Resposta correta";
+        resultado.style.color = "green"
+    } 
+    else{
+        resultado.innerHTML = "Resposta correta";
+        resultado.style.color = "red"
+    }
+}
+
+
+//Função pergunta 4
+function verificarResposta__4(resposta) {
+    if (resposta === "a"){
+        resultado.innerHTML = "Resposta correta";
+        resultado.style.color = "green"
+    } 
+    else{
+        resultado.innerHTML = "Resposta correta";
+        resultado.style.color = "red"
+    }
+}
+
+
